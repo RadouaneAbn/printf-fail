bf4384fa428ebb8c0680cd4bd8575cc227bb3c99
radouane.abounouas <radouaneabn@gmail.com>
2023-09-16 16:01:40 +0100

    using switch

--- printf.c
+++ printf.c
@@ -1,31 +1,27 @@
 #include "main.h"
 #include <unistd.h>
 #include <string.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdarg.h>
 
-int def(va_list args);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_i(va_list args);
-int print_d(va_list args);
+int print_i(int n);
+int print_d(int n);
 
 /**
-* _printf - this function prints anything
-*/
+ * _printf - this function prints anything
+ * @format: the string
+ * Return: 0
+ */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, j, c = 0, tmp_n;
+	char *s;
+	char a;
 
-	data op[] = {
-	{'s', print_s},
-	{'c', print_c},
-	{'d', print_d},
-	{'i', print_i},
-	{'%', def},
-	};
+	if (format == NULL)
+		return (0);
 
 	va_start(args, format);
 	for (i = 0; format[i]; i++)
@@ -34,53 +30,51 @@ int _printf(const char *format, ...)
 			write(1, &format[i], 1), c++;
 		else
 		{
-			for (j = 0; j < 5; j++)
+			i++;
+			switch (format[i])
 			{
-				if (format[i + 1] == op[j].s)
-				{
-					tmp_n = op[j].f(args);
-					i += 2;
-					c += tmp_n;
-				}
+				case 'c':
+					a = va_arg(args, int);
+					write(1, &a, 1);
+					c++;
+					break;
+				case 's':
+					j = 0;
+					s = va_arg(args, char *);
+					while (s[j])
+					{
+						write(1, &s[j], 1);
+						j++;
+						c++;
+					}
+					break;
+				case 'i':
+					tmp_n = va_arg(args, int);
+					c += print_i(tmp_n);
+					break;
+				case 'd':
+					tmp_n = va_arg(args, int);
+					c += print_d(tmp_n);
+					break;
+				case '%':
+					write(1, &format[i], 1);
+					c++;
+					break;
+				default:
+					write(1, &format[i - 1], 1);
+					write(1, &format[i], 1);
+					c += 2;
+					break;
+					c++;
 			}
-			write(1, &format[i], 1);
-			c++;
 		}
 	}
 	va_end(args);
 	return (c);
 }
 
-int def(va_list args)
+int print_i(int n)
 {
-	char tmp = va_arg(args, int);
-
-	if (tmp == '%')
-		write(1, &tmp, 1);
-	return (1);
-}
-
-int print_s(va_list args)
-{
-	int i;
-	char *s = va_arg(args, char*);
-
-	for (i = 0; s[i]; i++)
-		write(1, &s[i], 1);
-	return (i);
-}
-
-int print_c(va_list args)
-{
-	char c = va_arg(args, int);
-
-	write(1, &c, 1);
-	return (1);
-}
-
-int print_i(va_list args)
-{
-	int n = va_arg(args, int);
 	int m, count = 0, i, flag = 0;
 	char *A;
 
@@ -94,7 +88,7 @@ int print_i(va_list args)
 	A = (char *)malloc(count + flag);
 	if (A == NULL)
 		return (0);
-	
+
 	for (i = count + flag - 1; i >= 0; i--)
 	{
 		A[i] = n % 10 + '0';
@@ -106,36 +100,35 @@ int print_i(va_list args)
 	for (; i <= count; i++)
 		write(1, &A[i], 1);
 	free(A);
-	return(count);
+	return (count);
 }
 
-int print_d(va_list args)
+int print_d(int n)
 {
-	int n = va_arg(args, int);
 	int m, count = 0, i, flag = 0;
-        char *A;
+	char *A;
 
-        for (m = n; m != 0; count++)
-                m /= 10;
-        if (n < 0)
-        {
-                flag = 1;
-                n *= -1;
-        }
-        A = (char *)malloc(count + flag);
-        if (A == NULL)
-                return (0);
+	for (m = n; m != 0; count++)
+		m /= 10;
+	if (n < 0)
+	{
+		flag = 1;
+		n *= -1;
+	}
+	A = (char *)malloc(count + flag);
+	if (A == NULL)
+		return (0);
 
-        for (i = count + flag - 1; i >= 0; i--)
-        {
-                A[i] = n % 10 + '0';
-                n /= 10;
-        }
-        if (flag == 1)
-                A[0] = '-';
-        i = 0;
-        for (; i <= count; i++)
-                write(1, &A[i], 1);
-        free(A);
-        return(count);
+	for (i = count + flag - 1; i >= 0; i--)
+	{
+		A[i] = n % 10 + '0';
+		n /= 10;
+	}
+	if (flag == 1)
+		A[0] = '-';
+	i = 0;
+	for (; i <= count; i++)
+		write(1, &A[i], 1);
+	free(A);
+	return (count);
 }
