669070259dc542acae20bae69a8624395a514629
radouane.abounouas <radouaneabn@gmail.com>
2023-09-16 10:41:47 +0100

    test file

---
+++ test/d_main.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 21;
+	int b = 021;
+	int c = 0x21;
+	_printf("Enter a = %d, b = %d, c = %d\n", a, b, c);
+	printf("Enter a = %i, b = %i, c = %i\n", a, b, c);
+	printf("Enter a = %d, b = %d, c = %d\n", a, b, c);
+	return (0);
+}
