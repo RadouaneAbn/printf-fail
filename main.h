23333c1d53c3896606e164eb486a2d89e485d8cc
Errafai <errafaiachraf1923@gmail.com>
2023-09-17 03:03:33 -0700

    main.h

--- main.h
+++ main.h
@@ -10,5 +10,9 @@ typedef struct format
 } data;
 
 int _printf(const char *format, ...);
-
+int print_i(int n); 
+int print_d(int n); 
+int _num_char(unsigned int n, char cs, int flag);
+int _print_str(char *s);
+int _num_check(int n, char cs);
 #endif
