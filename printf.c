dd6932a2bd38dfe1da568b79b86e43896b09e2a4
radouane.abounouas <radouaneabn@gmail.com>
2023-09-17 12:49:35 +0100

    check 8

--- printf.c
+++ printf.c
@@ -187,11 +187,14 @@ int _print_char(char c)
 
 int _print_str(char *s)
 {
-	int i = 0;
+	int i;
 
 	if (s == NULL)
+	{
 		write(1, "(null)", 6);
-	for (; s[i]; i++)
+		return (6);
+	}
+	for (i = 0; s[i]; i++)
 		write(1, &s[i], 1);
 	return (i);
 }
