8840f36851bfee1ccafe96949ab4d4df1e343c84
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 16:01:05 -0700

    add functions

---
+++ _print_rev.c
@@ -0,0 +1,21 @@
+/**
+ * _print_rev - print a string in revers
+ * @s: the string
+ * Return: return the lenght of the string
+ */
+
+int _print_rev(char *s)
+{
+        int i, l;
+
+        l = _strlen(s);
+        if (s == NULL)
+        {
+                write(1, "(null)", 6);
+                return (6);
+        }
+        for (i = l - 1; i >= 0; i--)
+                write(1, &s[i], 1);
+        return (l);
+}
+
