ad38b1fbd7e91e612af8069e5320e0f29743eab5
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 16:11:36 -0700

    add functions

--- printf.c
+++ printf.c
@@ -184,26 +184,6 @@ int _print_rot(char *s)
 	return (i);
 }
 
-/**
- * _print_rev - print a string in revers
- * @s: the string
- * Return: return the lenght of the string
- */
-
-int _print_rev(char *s)
-{
-	int i, l;
-
-	l = _strlen(s);
-	if (s == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	for (i = l - 1; i >= 0; i--)
-		write(1, &s[i], 1);
-	return (l);
-}
 
 /**
  * _num_check - convert a negative number to positive
