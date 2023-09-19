b9858b0cb51569e5b274979cc6390dbcaeabda8d
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 15:59:00 -0700

    add functions

---
+++ _print_rot.c
@@ -0,0 +1,30 @@
+/**
+ * _print_rot - this function encodes a string using rot13
+ * @s: the string
+ * Return: return the lenght of the string
+ */
+
+int _print_rot(char *s)
+{
+        int i, j;
+
+        char *rot = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char *ebg = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+                        for (j = 0; j < 52; j++)
+                        {
+                                if (s[i] == rot[j])
+                                {
+                                        write(1, &ebg[j], 1);
+                                        break;
+                                }
+                        }
+                else
+                        write(1, &s[i], 1);
+        }
+        return (i);
+}
+
