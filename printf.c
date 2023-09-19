2829bdc3e16aea5a3e6855a9ad1d10b4bb2e47b7
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 16:15:25 -0700

    add functions

--- printf.c
+++ printf.c
@@ -156,24 +156,6 @@ int _printf(const char *format, ...)
 
 
 
-/**
- * _num_check - convert a negative number to positive
- * @n: the number
- * @cs: conversion specifiers
- * Return: the return of _num_char
- */
-
-int _num_check(long int n, char cs)
-{
-	int flag = 0;
-
-	if (n < 0)
-	{
-		flag = 1;
-		n *= -1;
-	}
-	return (_num_char(n, cs, flag));
-}
 
 /**
  * _num_char - convert a unsigned integer to string
