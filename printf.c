59fdab380a987c929065b90c05f16be4d46325ef
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 16:17:42 -0700

    add functions

--- printf.c
+++ printf.c
@@ -157,25 +157,6 @@ int _printf(const char *format, ...)
 
 
 
-/**
- * _print_str - print an array of characters
- * @s: the array (string)
- * Return: the number of characters
- */
-
-int _print_str(char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	for (i = 0; s[i]; i++)
-		write(1, &s[i], 1);
-	return (i);
-}
 
 
 /**
