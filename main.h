f09e89b711772249db17f4acb847ece54677cf4c
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 16:06:52 -0700

    add functions

--- main.h
+++ main.h
@@ -6,4 +6,12 @@
 
 int _printf(const char *format, ...);
 
+int _num_char(unsigned long int n, char cs, int flag);
+int _print_str(char *s);
+int _num_check(long int n, char cs);
+int _print_nonprintable(char *s);
+int _print_adresse(void *p);
+int _print_rot(char *s);
+int _print_rev(char *s);
+
 #endif
