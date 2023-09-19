25e0180260b6fccac8746dc3b36cac5d793cfc95
Errafai <errafaiachraf1923@gmail.com>
2023-09-19 15:47:39 -0700

    add functions

---
+++ _num_check.c
@@ -0,0 +1,19 @@
+/**
+ * _num_check - convert a negative number to positive
+ * @n: the number
+ * @cs: conversion specifiers
+ * Return: the return of _num_char
+ */
+
+int _num_check(long int n, char cs)
+{
+        int flag = 0;
+
+        if (n < 0)
+        {
+                flag = 1;
+                n *= -1;
+        }
+        return (_num_char(n, cs, flag));
+}
+
